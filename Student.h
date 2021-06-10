544bb63ab8faba9c47d7cfb7f9bd95bf3c0a74d1
Jeff Simon <jeffsdevinfo@gmail.com>
2021-06-10 12:54:55 -0700

    moved practice class out of main AVLTree file

---
+++ Student.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+
+class Student
+{
+public:
+	Student() {}
+	Student(int age, std::string name) : m_age(age), m_name(name) {}
+	Student(const Student& other)
+	{
+		m_age = other.m_age;
+		m_name = other.m_name;
+
+	}
+	Student& operator=(Student&& movedStudent)
+	{
+		m_age = movedStudent.m_age;
+		m_name = movedStudent.m_name;
+	}
+	int m_age;
+	std::string m_name;
+	friend std::ostream& operator<< (std::ostream& out, const Student& c) {
+		out << "\nName: " << c.m_name << "\tAge: " << c.m_age;
+		return out;
+	}
+};
